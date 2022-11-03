4586747d550a94a0e0d052132abb5d536aa23046
atilio.ferreira <atilio.ferreira@dtidigital.com.br>
2022-11-03 16:09:03 -0300

    configurando dragdrop

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,63 @@ import Grid from "@mui/material/Grid";
 import { Done } from "./components/columns/Done";
 import { InProgress } from "./components/columns/InProgress";
 import { ToDo } from "./components/columns/ToDo";
+import { useDispatch } from "react-redux";
+import { StoreDispatch } from "./redux/store";
+import { useAppSelector } from "./redux/hooks";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import { ToDoSlice as todo } from "./redux/slice/ToDoSlice";
+import { InProgressSlice as inProgress } from "./redux/slice/InProgressSlice";
+import { DoneSlice as done } from "./redux/slice/DoneSlice";
+import { IModel } from "./types";
+
+type TAllSlice = "todo" | "inProgress" | "done";
 
 function App() {
+  const dispatch = useDispatch<StoreDispatch>();
+  const appState = useAppSelector((state) => state);
+
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+
+    const { destination, source, draggableId } = result;
+    const allSlices = { todo, inProgress, done };
+
+    if (destination.droppableId === source.droppableId) {
+      dispatch(allSlices[destination.droppableId as TAllSlice].actions.reorder);
+    } else {
+      const [filterState] = (
+        (appState as any)[source.droppableId] as IModel[]
+      ).filter(({ id }) => id === draggableId);
+
+      dispatch(
+        allSlices[source.droppableId as TAllSlice].actions.remove(draggableId)
+      );
+      dispatch(
+        allSlices[destination.droppableId as TAllSlice].actions.update({
+          ...result,
+          filterState,
+        })
+      );
+    }
+  };
+
   return (
     <Container>
       <Typography textAlign="center" variant="h3" mt={3} mb={5}>
         ToDo App
       </Typography>
       <Grid container spacing={3} justifyContent="center">
-        <Grid item md={4}>
-          <ToDo />
-        </Grid>
-        <Grid item md={4}>
-          <InProgress />
-        </Grid>
-        <Grid item md={4}>
-          <Done />
-        </Grid>
+        <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
+          <Grid item md={4}>
+            <ToDo />
+          </Grid>
+          <Grid item md={4}>
+            <InProgress />
+          </Grid>
+          <Grid item md={4}>
+            <Done />
+          </Grid>
+        </DragDropContext>
       </Grid>
     </Container>
   );
